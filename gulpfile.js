e42ecdf4d4b034f5ac467a3253fee7835891c0db
pudilz <zack.pudil@dowjones.com>
2016-01-03 14:09:55 -0600

    Removed unneded dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
-var concat = require("gulp-concat");
 var browserify = require("gulp-browserify");
 
 gulp.task("babelify", function () {
